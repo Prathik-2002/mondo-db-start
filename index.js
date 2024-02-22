15bb90c940ade86c2f34b1196114c9013f26b141
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-22 10:03:36 +0530

    mongoose is used to connect with mongoDB

--- index.js
+++ index.js
@@ -1,149 +1,98 @@
-const {MongoClient} = require('mongodb');
-const URI = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
+const Book = require('./schema');
 
-// const testDB = 'Test';
-const DemoDB = 'Demo';
-
-const addNewBook = async (Library, book) => {
+const connectToMongoDB =async (URI) => {
   try {
-    await Library.insertOne(book);
-    console.log('Added book');
+    await mongoose.connect(URI);
+    return true;
   } catch (err) {
-    console.log('Unable to insert');
+    console.log('Unable to connect');
+    return false;
+  }
+};
+const closeConnection = async () => {
+  await mongoose.disconnect();
+  console.log('Connection closed');
+};
+const addNewBook = async (id, name, author, pages, genre, publisher, age) => {
+  try {
+    await Book.create({
+      bookId: id,
+      bookName: name,
+      author: author,
+      pages: pages,
+      genre: genre,
+      publisher: publisher,
+      age: age,
+    });
+    return true;
+  } catch (err) {
+    console.log('Insertion failed');
+    return false;
   }
 };
 
-const getBookByAuthor = async (Library, authorName) => {
+const changePublisherById = async (id, newPublisher) => {
   try {
-    const books = await Library.find({author: authorName});
-    const booksList = await books.toArray();
-    if ( booksList.length > 0 ) {
-      console.table(booksList);
-    } else {
-      console.log('No Books found');
+    const isExists = await Book.exists({bookId: id});
+    if (isExists === null) {
+      throw new 'Book does not exist';
     }
+    await Book.updateMany({bookId: id}, {$set: {publisher: newPublisher}});
+    return true;
   } catch (err) {
-    console.log('Unable to find books');
+    console.log('Update failed');
+    return false;
+  }
+};
+const deleteBookById = async (id) => {
+  try {
+    const isExists = await Book.exists({bookId: id});
+    if (isExists === null) {
+      throw new 'Book does not exist';
+    }
+    await Book.deleteMany().where('bookId').equals(id);
+    return true;
+  } catch (err) {
+    console.log('Delete failed');
+    return false;
+  }
+};
+const getAllBooks = async () => {
+  try {
+    const books = await Book.find();
+    return books;
+  } catch (err) {
+    console.log('Failed to get all books');
+    return null;
+  }
+};
+const getBookByAuthor = async (author) => {
+  try {
+    const books = await Book.find().where('author').equals(author);
+    return books;
+  } catch (err) {
+    console.log('Unable to get Book');
+    return null;
+  }
+};
+const incrementBookAgeById = async (id) => {
+  try {
+    await Book.updateOne({bookId: id}, {$inc: {age: 1}});
+    return true;
+  } catch (err) {
+    console.log('unable to increment');
+    return false;
   }
 };
 
-const displayAllBooks = async (Library) => {
-  try {
-    const books = await Library.find({});
-    return await books.toArray();
-  } catch (err) {
-    console.log('Unable to display books');
-  }
+module.exports = {
+  connectToMongoDB,
+  closeConnection,
+  addNewBook,
+  getAllBooks,
+  getBookByAuthor,
+  changePublisherById,
+  incrementBookAgeById,
+  deleteBookById,
 };
-
-const ChangeBookPublicationById = async (Library, id, newPublication) => {
-  try {
-    await Library.updateMany({bookId: id}, {$set: {publication: newPublication}});
-    console.log(`Book ${id} Publication changed to ${newPublication}`);
-  } catch (err) {
-    console.log('Unable to Change publication');
-  }
-};
-
-const IncrementBookAgeById = async (Library, id) => {
-  try {
-    await Library.updateMany({bookId: id}, {$inc: {age: 1}});
-    console.log(`Book ${id} age incremented`);
-  } catch (err) {
-    console.log('Unable to update age');
-  }
-};
-
-const getBookCountByBookName = async (Library, bookName) => {
-  try {
-    const books = await Library.find({bookName: bookName});
-    const bookslist = await books.toArray();
-    return bookslist.length;
-  } catch (err) {
-    console.log('Unable to count books');
-  }
-};
-
-const deleteBookById = async (Library, bookId) => {
-  try {
-    await Library.deleteMany({bookId: bookId});
-    console.log(`Book ${bookId} deleted successfully`);
-  } catch (err) {
-    console.log('Unable to delete book');
-  }
-};
-
-
-async function start() {
-  const client = new MongoClient(URI);
-  try {
-    await client.connect();
-    console.log('Connection successfully established');
-    const database = client.db(DemoDB);
-    const collectionName = 'Library';
-    const Library = database.collection(collectionName);
-    Library.createIndex({bookId: 1}, {unique: true});
-    await addNewBook(Library, {
-      bookId: 1216,
-      bookName: 'The Jungle Book',
-      author: 'Rudyard Kipling',
-      pageCount: '409',
-      genre: ['Adventure', 'Fiction'],
-      publication: 'Macmillan',
-      age: 3,
-    });
-    await addNewBook(Library, {
-      bookId: 3213,
-      bookName: 'The Jungle Book',
-      author: 'Rudyard Kipling',
-      pageCount: '409',
-      genre: ['Adventure', 'Fiction'],
-      publication: 'Macmillan',
-      age: 3,
-    });
-    await addNewBook(Library, {
-      bookId: 5623,
-      bookName: 'Pennywise',
-      author: 'Stephan King',
-      pageCount: '890',
-      genre: ['Horror', 'Thriller'],
-      publication: 'Simon & Schuster',
-      age: 7,
-    });
-    await addNewBook(Library, {
-      bookId: 2323,
-      bookName: 'Hunger Games',
-      author: 'Suzanne Collins',
-      pageCount: '898',
-      genre: ['Adventure', 'Fiction', 'Sci-Fi'],
-      publication: 'Scholastic',
-      age: 2,
-    });
-    await addNewBook(Library, {
-      bookId: 8344,
-      bookName: 'Pride and Prejudice',
-      author: 'Jane Austen',
-      pageCount: '490',
-      genre: ['Romance'],
-      publication: 'Simon & Schuster',
-      age: 1,
-    });
-    console.table(await displayAllBooks(Library));
-
-    console.log('\nJane Austen Books: ');
-    await getBookByAuthor(Library, 'Jane Austen');
-    const noOfbooks = await getBookCountByBookName(Library, 'The Jungle Book');
-    console.log('\nThe jungle Book count is: ' + noOfbooks);
-    await deleteBookById(Library, 2323);
-    console.table(await displayAllBooks(Library));
-    await IncrementBookAgeById(Library, 8344);
-    console.table(await displayAllBooks(Library));
-    await ChangeBookPublicationById(Library, 8344, 'Jack Smith');
-    console.table(await displayAllBooks(Library));
-  } catch (e) {
-    console.log('Unable to Connect to the mongoDB');
-  } finally {
-    client.close();
-  }
-}
-start();
