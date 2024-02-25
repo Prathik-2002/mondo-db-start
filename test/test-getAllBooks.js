296763df915c5ff09fd68200e7ac04e819d73b52
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-25 22:31:50 +0530

    populate separated

--- test/test-getAllBooks.js
+++ test/test-getAllBooks.js
@@ -1,45 +1,14 @@
 const mongoose = require('mongoose');
 const assert = require('assert');
-const {addNewBook, closeConnection, connectToMongoDB, getAllBooks} = require('../index');
+const {closeConnection, connectToMongoDB, getAllBooks} = require('../index');
+const {populate} = require('./polulate');
+
 const URIs = {
   validURI: {uri: 'mongodb://localhost/Demo', connectionResult: true},
   invalidURI: {uri: 'mongod://localhost/Demo', connectionResult: false},
   testURI: {uri: 'mongodb://localhost/Test', connectionResult: true},
 };
-const populate = async () => {
-  await addNewBook('1234',
-      'The Great Gatsby',
-      'F. Scott Fitzgerald',
-      180,
-      ['Fiction', 'Classics'],
-      'Scribner',
-      18,
-  );
-  await addNewBook('1235',
-      'The Great Gatsby',
-      'F. Scott Fitzgerald',
-      180,
-      ['Fiction', 'Classics'],
-      'Scribner',
-      18,
-  );
-  await addNewBook('1236',
-      'The Great Gatsby',
-      'F. Scott Fitzgerald',
-      180,
-      ['Fiction', 'Classics'],
-      'Scribner',
-      18,
-  );
-  await addNewBook('5678',
-      'Pride and Prejudice',
-      'Jane Austen',
-      279,
-      ['Fiction', 'Romance', 'Classics'],
-      'T. Egerton, Whitehall',
-      16,
-  );
-};
+
 const expectedoutputIds = ['1234', '1235', '1236', '5678'];
 describe('getAllBooks test', ()=> {
   describe('Test with mongoDB connection', ()=> {
